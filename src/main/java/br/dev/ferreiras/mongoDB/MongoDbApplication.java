ecb97b0a5a4f35ecf7063982c03e23927b1c01f6
Ricardo Ferreira <rnhc1000@hotmail>
2025-02-13 11:58:20 -0300

    [feat] ✨: initial commit - dependencies defined at pom.xml

---
+++ src/main/java/br/dev/ferreiras/mongoDB/MongoDbApplication.java
@@ -0,0 +1,42 @@
+package br.dev.ferreiras.mongoDB;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+@SpringBootApplication
+public class MongoDbApplication implements CommandLineRunner {
+public static final Logger logger = LoggerFactory.getLogger(MongoDbApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbApplication.class, args);
+	}
+
+	/**
+	 * @param args incoming main method arguments
+	 * @throws Exception on error
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+
+		final LocalDateTime localTime = LocalDateTime.now();
+		final ZonedDateTime zonedDateTime = ZonedDateTime.now(ZonedDateTime.now().getZone());
+		final String javaVersion = System.getProperty("java.version");
+		final int  numOfCores = Runtime.getRuntime().availableProcessors();
+
+		if (MongoDbApplication.logger.isInfoEnabled()) {
+			MongoDbApplication.logger.info("MongoDB Restful API started running at {}, zone {}, running java version {}, on top of {} cores",
+					localTime,
+					zonedDateTime,
+					javaVersion,
+					numOfCores
+			);
+		}
+
+	}
+}
